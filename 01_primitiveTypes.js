f86b35cdfb751861d2e63a2498a1c41efcee030a
Prasadkare77 <prasadkare77@gmail.com>
2023-05-06 09:01:02 +0530

    Data Types in JavaScript
    Data Types in JavaScript

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,38 @@
+
+//Primitive Data Types
+//01. Number
+var age = 30;
+var weight = 62;
+var height = 5.5;
+var speckNumber = 0.50;
+
+console.log(age,weight,height,speckNumber);
+
+console.log(typeof age);
+
+//02. Boolean --> true or false
+
+var isMarried = true;
+var isSoftEngg = true;
+var batchelor = false;
+console.log(isMarried,isSoftEngg,batchelor);
+console.log(typeof isMarried);
+
+//03. String --> Sequence of character
+var firstName = "Rohit";
+var lastName = "Shrama";
+console.log(firstName, lastName);
+console.log(typeof firstName);
+
+//Special Data Types
+//01. Undefined
+var city = " ";
+console.log(typeof city);
+
+var address;
+console.log(typeof address);
+
+//02. Null
+var pin = null;
+console.log("Valaue: ", pin);
+console.log("Type: ", typeof pin);
---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Prasad Kare.");
+console.log("Address: Malshiras, Dist - Solapur, Maharashtra");
+
+var firstname = "Prasad";
+console.log(firstname);
+
+var holder;
+console.log(holder);
+holder = "Bucket";
+console.log(holder);
+
+holder = "List";
+console.log(holder);
