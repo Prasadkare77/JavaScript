d647bab8407acc277a58c3aa943c9a1a4e91fd88
Prasadkare77 <prasadkare77@gmail.com>
2024-06-13 07:58:14 +0530

    updated

--- practice.js
+++ practice.js
@@ -1,16 +1,14 @@
-function fibbonacci(num){
-    let num1= 0
-    let num2 = 1
-    let sum =0;
-    let fibb = ''
-    let first = num1
-    let second = num2
-    for(let i=0; i<num; i++){
-        sum = num1 + num2
-        num1 = num2
-        num2 = sum
-        fibb = fibb + sum + ' '
-    }
-    console.log(first, second,fibb)
+const val = [12,43,57,66,44,100,201,157];
+let evenCount = 0;
+let oddCount = 0;
+
+for(let i=0; i<val.length; i++){
+  if (val[i] % 2 == 0) {
+    evenCount++
+  } else {
+    oddCount++
+  }
 }
-fibbonacci(10)
+
+console.log(evenCount)
+console.log(oddCount)
